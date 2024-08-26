e76c4af3a3b0a6272cdcb4df19eba1229e6ab77a
Julio Developer <juliodeveloper777@gmail.com>
2024-08-25 21:45:47 -0300

    feat/fix: visual pages and components

--- src/app/(low-racing)/dashboard/page.tsx
+++ src/app/(low-racing)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/app/components/header";
 
 export default function Dashboard() {
   return (
-    <div className="bg-[#1B1B1B]">
+    <div className="h-screen w-screen bg-[#1B1B1B]">
       <Header />
     </div>
   );
--- src/app/(low-racing)/login/page.tsx
+++ src/app/(low-racing)/login/page.tsx
@@ -3,7 +3,7 @@ import FormLogin from "@/app/components/form-login";
 
 export default function Login() {
   return (
-    <div className="relative h-screen bg-image-bg bg-cover bg-center bg-no-repeat">
+    <div className="relative h-screen w-screen bg-image-bg bg-cover bg-center bg-no-repeat">
       <Assets />
       <FormLogin />
     </div>
