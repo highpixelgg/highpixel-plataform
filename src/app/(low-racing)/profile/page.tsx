ef5c208d628a38d8cbb59ae17360a75bb451aaf7
Willyan Costa <willyancr@gmail.com>
2024-09-03 21:13:36 -0300

    feat: create page profile

---
+++ src/app/(low-racing)/profile/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/app/components/header";
+import ProfileContent from "@/app/components/profile";
+
+export default function PageProfile() {
+  return (
+    <div className="h-screen w-screen bg-[#1B1B1B]">
+      <Header />
+      <ProfileContent />
+    </div>
+  );
+}
