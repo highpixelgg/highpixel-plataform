30431dcee43c895a9c92f1cc31f7baeaf2934a3f
Julio Developer <juliodeveloper777@gmail.com>
2025-01-06 22:54:26 -0300

    fix: fix text button post

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -21,7 +21,7 @@ export const Button = ({
 }: Props) => {
   return (
     <button
-      className={`--font-rubik flex cursor-pointer items-center justify-center rounded-[10px] p-3 text-xl font-medium text-black transition-all hover:brightness-75 ${size === 1 && "h-14 text-lg"} ${size === 2 && "inline-block h-10 w-[120px] text-[15px] lg:w-32 lg:text-base"} ${size === 3 && "size-11 text-xs font-bold"} ${size === 4 && "h-7 w-20 text-xs font-bold"} ${borderounded === true && "rounded-full"} ${background === "white" && "bg-[#FFFFFF]"} ${background === "transparent" && "bg-transparent"}`}
+      className={`--font-rubik flex cursor-pointer items-center justify-center rounded-[10px] p-3 text-xl font-medium text-black transition-all hover:brightness-75 ${size === 1 && "h-14 text-lg"} ${size === 2 && "inline-block h-10 w-[120px] text-[14px] lg:w-32 lg:text-base"} ${size === 3 && "size-11 text-xs font-bold"} ${size === 4 && "h-7 w-20 text-xs font-bold"} ${borderounded === true && "rounded-full"} ${background === "white" && "bg-[#FFFFFF]"} ${background === "transparent" && "bg-transparent"}`}
       onClick={onClick}
     >
       {icon === "link" && <ExternalLink size={40} color="#c8c8c8" />}
